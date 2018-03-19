85910033aa7ba00f96d2ee9ceff50aaa8d017d0d
UnlikePluto <sv.simikin@gmail.com>
2018-03-19 18:04:46 +0100

    -Fixed use of deprecated interface PropertyState<T>, replaced by Property<T> -Fixed use of deprecated interface Project.property(Class<T>), replaced by ObjectFactory.property(java.lang.Class)

--- src/main/java/de/heinrichmarkus/gradle/delphi/tasks/Clean.java
+++ src/main/java/de/heinrichmarkus/gradle/delphi/tasks/Clean.java
@@ -2,14 +2,14 @@ package de.heinrichmarkus.gradle.delphi.tasks;
 
 import de.heinrichmarkus.gradle.delphi.utils.Utils;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.provider.PropertyState;
+import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.File;
 
 public class Clean extends DefaultTask {
-    private final PropertyState<String> binDirectory = getProject().property(String.class);
+    private final Property<String> binDirectory = getProject().getObjects().property(String.class);
 
     @TaskAction
     public void cleanup() {
@@ -24,7 +24,7 @@ public class Clean extends DefaultTask {
     }
 
     @Input
-    public PropertyState<String> getBinDirectory() {
+    public Property<String> getBinDirectory() {
         return binDirectory;
     }
 }
